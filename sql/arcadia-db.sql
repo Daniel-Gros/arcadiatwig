a5e3ad5b44a3dcd344c941f6acd08558ef27d2b2
Daniel Gros <danielgrosbgns@gmail.com>
2024-11-06 22:24:38 +0100

    correction des id de tables

--- sql/arcadia-db.sql
+++ sql/arcadia-db.sql
@@ -1,4 +1,4 @@
--- Création de la base de données arcadiadb
+-- Création de la base de données arcadiad
 CREATE DATABASE IF NOT EXISTS arcadiadb;
 
 USE arcadiadb;
@@ -6,7 +6,7 @@ USE arcadiadb;
 -- Création des tables
 CREATE TABLE
     IF NOT EXISTS user (
-        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(50) NOT NULL,
         password VARCHAR(50) NOT NULL,
         email VARCHAR(50) NOT NULL
@@ -14,7 +14,7 @@ CREATE TABLE
 
 CREATE TABLE
     IF NOT EXISTS animal (
-        animal_id INT AUTO_INCREMENT PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY,
         firstName VARCHAR(50) NOT NULL,
         breed VARCHAR(50) NOT NULL,
         diet VARCHAR(50) NOT NULL
@@ -22,28 +22,28 @@ CREATE TABLE
 
 CREATE TABLE
     IF NOT EXISTS service (
-        service_id INT AUTO_INCREMENT PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(50) NOT NULL,
         description TEXT NOT NULL
     );
 
 CREATE TABLE
     IF NOT EXISTS habitat (
-        habitat_id INT AUTO_INCREMENT PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(50) NOT NULL,
         description TEXT NOT NULL
     );
 
 CREATE TABLE
     IF NOT EXISTS CompteRenduVétérinaire (
-        compte_rendu_vet_id INT AUTO_INCREMENT PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY,
         date DATE NOT NULL,
         message TEXT NOT NULL
     );
 
 CREATE TABLE
     IF NOT EXISTS avis (
-        avis_id INT AUTO_INCREMENT PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY,
         note INT NOT NULL,
         message TEXT NOT NULL,
         validation BOOLEAN NOT NULL
@@ -51,9 +51,9 @@ CREATE TABLE
 
 CREATE TABLE
     IF NOT EXISTS CommentaireHabitat (
-        commentaire_hab_id INT AUTO_INCREMENT PRIMARY KEY,
+        id INT AUTO_INCREMENT PRIMARY KEY,
         message TEXT NOT NULL,
-        imrovemment BOOLEAN NOT NULL
+        improvement BOOLEAN NOT NULL
     );
 
 -- Création des clés étrangères
@@ -61,40 +61,40 @@ USE arcadiadb;
 
 ALTER TABLE animal ADD user_id INT;
 
-ALTER TABLE animal ADD FOREIGN KEY (user_id) REFERENCES user (user_id);
+ALTER TABLE animal ADD FOREIGN KEY (user_id) REFERENCES user (id);
 
 ALTER TABLE animal ADD habitat_id INT;
 
-ALTER TABLE animal ADD FOREIGN KEY (habitat_id) REFERENCES habitat (habitat_id);
+ALTER TABLE animal ADD FOREIGN KEY (habitat_id) REFERENCES habitat (id);
 
 ALTER TABLE service
 ADD COLUMN user_id INT;
 
-ALTER TABLE service ADD FOREIGN KEY (user_id) REFERENCES user (user_id);
+ALTER TABLE service ADD FOREIGN KEY (user_id) REFERENCES user (id);
 
 ALTER TABLE habitat ADD user_id INT;
 
-ALTER TABLE habitat ADD FOREIGN KEY (user_id) REFERENCES user (user_id);
+ALTER TABLE habitat ADD FOREIGN KEY (user_id) REFERENCES user (id);
 
 ALTER TABLE CompteRenduVétérinaire ADD user_id INT;
 
 ALTER TABLE CompteRenduVétérinaire ADD animal_id INT;
 
-ALTER TABLE CompteRenduVétérinaire ADD FOREIGN KEY (user_id) REFERENCES user (user_id);
+ALTER TABLE CompteRenduVétérinaire ADD FOREIGN KEY (user_id) REFERENCES user (id);
 
-ALTER TABLE CompteRenduVétérinaire ADD FOREIGN KEY (animal_id) REFERENCES animal (animal_id);
+ALTER TABLE CompteRenduVétérinaire ADD FOREIGN KEY (animal_id) REFERENCES animal (id);
 
 ALTER TABLE avis ADD user_id INT;
 
-ALTER TABLE avis ADD FOREIGN KEY (user_id) REFERENCES user (user_id);
+ALTER TABLE avis ADD FOREIGN KEY (user_id) REFERENCES user (id);
 
 ALTER TABLE CommentaireHabitat ADD user_id INT;
 
 ALTER TABLE CommentaireHabitat ADD habitat_id INT;
 
-ALTER TABLE CommentaireHabitat ADD FOREIGN KEY (user_id) REFERENCES user (user_id);
+ALTER TABLE CommentaireHabitat ADD FOREIGN KEY (user_id) REFERENCES user (id);
 
-ALTER TABLE CommentaireHabitat ADD FOREIGN KEY (habitat_id) REFERENCES habitat (habitat_id);
+ALTER TABLE CommentaireHabitat ADD FOREIGN KEY (habitat_id) REFERENCES habitat (id);
 
 -- Insertion des données
 INSERT INTO user
