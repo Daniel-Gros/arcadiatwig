eb6951b2cf191bb58ae26b00d11aa968b7333cca
Daniel Gros <danielgrosbgns@gmail.com>
2024-11-10 17:32:32 +0100

    Bootstrap js fonctionne enfin mazeltov

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,10 @@
 import './vendor/bootstrap/bootstrap.index.js';
-
-import './styles/main.sass';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+import './styles/app.scss';
 
 import './vendor/bootstrap/dist/css/bootstrap.min.css';
