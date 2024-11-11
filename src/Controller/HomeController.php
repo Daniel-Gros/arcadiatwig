828d07d8dcb43dced14d89ad4125c7b5d89c690d
Daniel Gros <danielgrosbgns@gmail.com>
2024-11-11 17:01:31 +0100

    création extension twig pour navbar + correction card animal

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,50 +13,50 @@ class HomeController extends AbstractController
     {
 
        // NAVBAR CONFIGUREE AVEC LES FUTURES ROUTES DE TOUTES LES PAGES
-        $navItems = [
-            [
-                'label' => 'Découvrir notre Zoo', 
-                'submenu' => [
-                    ['label' => 'Animaux', 'route' => 'app_animal'],
-                    ['label' => 'Habitats', 'route' => 'app_habitat'],
-                    ['label' => 'Engagements', 'route' => 'app_engagements']
-                ]
-            ],
-            [
-                'label' => 'Venir au Zoo', 
-                'submenu' => [
-                    ['label' => 'Accès', 'route' => 'app_acces'],
-                    ['label' => 'Horaires', 'route' => 'app_horaires'],
-                    ['label' => 'Plan du Zoo', 'route' => 'app_plan_du_zoo'],
-                    ['label' => 'Restaurants', 'route' => 'app_restaurants']
-                ]
-            ],
-            [
-                'label' => 'Aventures', 
-                'submenu' => [
-                    ['label' => 'Le Potitrain', 'route' => 'app_potitrain'],
-                    ['label' => 'Animations soigneurs', 'route' => 'app_animations_soigneurs'],
-                    ['label' => 'Fêter son anniversaire', 'route' => 'app_anniversaire']
-                ]
-            ],
-            [
-                'label' => 'Professionnels', 
-                'submenu' => [
-                    ['label' => 'Sortie scolaire', 'route' => 'app_sortie_scolaire'],
-                    ['label' => 'Groupes', 'route' => 'app_groupes'],
-                    ['label' => 'CE et collectivités', 'route' => 'app_ce_collectivites'],
-                    ['label' => 'Séminaire', 'route' => 'app_seminaire']
-                ]
-            ],
-            ['label' => 'Avis', 'route' => 'app_avis'],
-            ['label' => 'Contact', 'route' => 'app_contact'],
-            ['label' => 'Connexion', 'route' => 'app_login']
-        ];
+        // $navItems = [
+        //     [
+        //         'label' => 'Découvrir notre Zoo', 
+        //         'submenu' => [
+        //             ['label' => 'Animaux', 'route' => 'app_animal'],
+        //             ['label' => 'Habitats', 'route' => 'app_habitat'],
+        //             ['label' => 'Engagements', 'route' => 'app_engagements']
+        //         ]
+        //     ],
+        //     [
+        //         'label' => 'Venir au Zoo', 
+        //         'submenu' => [
+        //             ['label' => 'Accès', 'route' => 'app_acces'],
+        //             ['label' => 'Horaires', 'route' => 'app_horaires'],
+        //             ['label' => 'Plan du Zoo', 'route' => 'app_plan_du_zoo'],
+        //             ['label' => 'Restaurants', 'route' => 'app_restaurants']
+        //         ]
+        //     ],
+        //     [
+        //         'label' => 'Aventures', 
+        //         'submenu' => [
+        //             ['label' => 'Le Potitrain', 'route' => 'app_potitrain'],
+        //             ['label' => 'Animations soigneurs', 'route' => 'app_animations_soigneurs'],
+        //             ['label' => 'Fêter son anniversaire', 'route' => 'app_anniversaire']
+        //         ]
+        //     ],
+        //     [
+        //         'label' => 'Professionnels', 
+        //         'submenu' => [
+        //             ['label' => 'Sortie scolaire', 'route' => 'app_sortie_scolaire'],
+        //             ['label' => 'Groupes', 'route' => 'app_groupes'],
+        //             ['label' => 'CE et collectivités', 'route' => 'app_ce_collectivites'],
+        //             ['label' => 'Séminaire', 'route' => 'app_seminaire']
+        //         ]
+        //     ],
+        //     ['label' => 'Avis', 'route' => 'app_avis'],
+        //     ['label' => 'Contact', 'route' => 'app_contact'],
+        //     ['label' => 'Connexion', 'route' => 'app_login']
+        // ];
         
         
         $website = 'Arcadia';
         return $this->render('home/index.html.twig', [
-            'navItems' => $navItems,
+            // 'navItems' => $navItems,
             'website' => $website,
             'controller_name' => 'HomeController',
         ]);
---
+++ src/Twig/NavbarExtension.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class NavbarExtension extends AbstractExtension
+{
+    private $navItems;
+
+    public function __construct()
+    {
+        $this->navItems = [
+            [
+                'label' => 'Découvrir notre Zoo', 
+                'submenu' => [
+                    ['label' => 'Animaux', 'route' => 'app_animal'],
+                    ['label' => 'Habitats', 'route' => 'app_habitat'],
+                    ['label' => 'Engagements', 'route' => 'app_engagements'],
+                    ['label' => 'Liste des services', 'route' => 'app_service']
+                ]
+            ],
+            [
+                'label' => 'Venir au Zoo', 
+                'submenu' => [
+                    ['label' => 'Accès', 'route' => 'app_acces'],
+                    ['label' => 'Horaires', 'route' => 'app_horaires'],
+                    ['label' => 'Plan du Zoo', 'route' => 'app_plan_du_zoo'],
+                    ['label' => 'Restaurants', 'route' => 'app_restaurants']
+                ]
+            ],
+            [
+                'label' => 'Aventures', 
+                'submenu' => [
+                    ['label' => 'Le Potitrain', 'route' => 'app_potitrain'],
+                    ['label' => 'Animations soigneurs', 'route' => 'app_animations_soigneurs'],
+                    ['label' => 'Fêter son anniversaire', 'route' => 'app_anniversaire']
+                ]
+            ],
+            [
+                'label' => 'Professionnels', 
+                'submenu' => [
+                    ['label' => 'Sortie scolaire', 'route' => 'app_sortie_scolaire'],
+                    ['label' => 'Groupes', 'route' => 'app_groupes'],
+                    ['label' => 'CE et collectivités', 'route' => 'app_ce_collectivites'],
+                    ['label' => 'Séminaire', 'route' => 'app_seminaire']
+                ]
+            ],
+            ['label' => 'Avis', 'route' => 'app_avis'],
+            ['label' => 'Contact', 'route' => 'app_contact'],
+            ['label' => 'Connexion', 'route' => 'app_login']
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('nav_items', [$this, 'getNavItems']),
+        ];
+    }
+
+    public function getNavItems(): array
+    {
+        return $this->navItems;
+    }
+}
