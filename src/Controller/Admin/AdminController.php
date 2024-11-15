8b9efd5e56b48252ef753597b6fbf4635e518040
Daniel Gros <danielgrosbgns@gmail.com>
2024-11-15 20:29:50 +0100

    migration de données feature de statistiques pour panneau administrateur + trad fr du crud user

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,25 +2,47 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\AnimalRepository;
+use App\Repository\HabitatRepository;
+use App\Repository\ServiceRepository;
+use App\Repository\AvisRepository;
+use App\Repository\CommentaireHabitatRepository;
+use App\Repository\CompteRenduVeterinaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
     #[IsGranted('ROLE_ADMIN')]
-    public function index(): Response
-    {
 
-        // $user = $this->getUser();
-        // $role = $user->getRole();
+
+    public function index(
+        AnimalRepository $animalRepository,
+        HabitatRepository $habitatRepository,
+        CommentaireHabitatRepository $commentaireHabitatRepository,
+        CompteRenduVeterinaireRepository $compteRenduVeterinaireRepository,
+        ServiceRepository $serviceRepository,
+        AvisRepository $avisRepository
+    ): Response {
+
+        $animalCount = $animalRepository->count([]);
+        $habitatCount = $habitatRepository->count([]);
+        $commentaireHabitatCount = $commentaireHabitatRepository->count([]);
+        $compteRenduVeterinaireCount = $compteRenduVeterinaireRepository->count([]);
+        $serviceCount = $serviceRepository->count([]);
+        $avisCount = $avisRepository->count([]);
 
         return $this->render('admin/dashboard/index.html.twig', [
             'controller_name' => 'AdminController',
-            //  'user' => $user,
-            // 'role' => $role
+            'animalCount' => $animalCount,
+            'habitatCount' => $habitatCount,
+            'commentaireHabitatCount' => $commentaireHabitatCount,
+            'compteRenduVeterinaireCount' => $compteRenduVeterinaireCount,
+            'serviceCount' => $serviceCount,
+            'avisCount' => $avisCount,
         ]);
     }
 }
