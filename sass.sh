71735d4911a3a6a931952977167ffccaa35083f1
Daniel Gros <danielgrosbgns@gmail.com>
2024-11-15 22:26:41 +0100

    scss fix avec commande bash

---
+++ sass.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+ # Compile SASS to CSS CLASSER LES FICHIERS PAR ORDRE ALPHABÉTIQUE
+
+sass assets/styles/sass/_base.scss assets/styles/css/_base.css
+sass assets/styles/sass/_reset.scss assets/styles/css/_reset.css
+sass assets/styles/sass/_typography.scss assets/styles/css/_typography.css
+
+
+
+sass assets/styles/sass/_button.scss assets/styles/css/_button.css
+sass assets/styles/sass/_card.scss assets/styles/css/_card.css
+sass assets/styles/sass/_form.scss assets/styles/css/_form.css
+sass assets/styles/sass/_navbar.scss assets/styles/css/_navbar.css
+sass assets/styles/sass/_stars.scss assets/styles/css/_stars.css
+
+
+
+sass assets/styles/sass/_functions.scss assets/styles/css/_functions.css
+sass assets/styles/sass/_mixins.scss assets/styles/css/_mixins.css
+sass assets/styles/sass/_variables.scss assets/styles/css/_variables.css
+
+sass assets/styles/sass/main.scss assets/styles/css/main.css 
+
+# chmod +x sass.sh
+# ./sass.sh
