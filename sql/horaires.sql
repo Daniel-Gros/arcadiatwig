2222b512bc1df31372e6e7afb80cd393928fa20c
Daniel Gros <danielgrosbgns@gmail.com>
2025-02-01 17:31:40 +0100

    add fichier SQL pour les horaires fichier d'exemple

---
+++ sql/horaires.sql
@@ -0,0 +1,10 @@
+use arcadiadb;
+
+INSERT INTO horaires (id, day, open, close) VALUES
+(1, 'Lundi', '08:00', '19:00'),
+(2, 'Mardi', '08:00', '19:00'),
+(3, 'Mercredi', '08:00', '19:00'),
+(4, 'Jeudi', '08:00', '19:00'),
+(5, 'Vendredi', '08:00', '19:00'),
+(6, 'Samedi', '08:00', '19:00'),
+(7, 'Dimanche', '08:00', '19:00');
